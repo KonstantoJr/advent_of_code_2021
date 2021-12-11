eb632220d368b1dc4b3bff865e19ee733b99179b
KonstantoJr <konstantokon29@gmail.com>
2021-12-11 13:12:30 +0200

    Delete Day_1 directory

--- Day_1/Day_1_part1.py
+++
@@ -1,8 +0,0 @@
-with open("Day_1/input.txt", "r") as file:
-    increase = -1
-    prev = -1
-    for i in file:
-        if int(i) > prev:
-            increase += 1
-        prev = int(i)
-print(increase)
