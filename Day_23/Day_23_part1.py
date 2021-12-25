79bf32e9827668eca992e6806162733a917683e5
KonstantoJr <konstantokon29@gmail.com>
2021-12-26 00:50:58 +0200

    Add files via upload

---
+++ Day_23/Day_23_part1.py
@@ -0,0 +1,13 @@
+grid = []
+with open("input.txt", "r") as f:
+    for i in f:
+        grid.append(i.strip())
+
+amoves = 0
+apoints = 1
+bmoves = 0
+bpoints = 10
+cmoves = 0
+cpoints = 100
+dmoves = 0
+dpoints = 1000
